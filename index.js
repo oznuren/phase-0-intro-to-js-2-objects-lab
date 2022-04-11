86786046b3b6a9e7afbbe39974e3d9ad51bb8aa3
Öznur <oznur@tuta.io>
2022-04-11 22:48:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let employee = {
+  name: "Sam",
+  streetAddress: "Theme street",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  let updateEmployeeWithKeyAndValueNew = { ...employee };
+  updateEmployeeWithKeyAndValueNew.streetAddress = "11 Broadway";
+  return updateEmployeeWithKeyAndValueNew;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee.streetAddress = "12 Broadway";
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  let newDeletedList = { ...employee };
+  delete newDeletedList.name;
+  return newDeletedList;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee.name;
+  return employee;
+}
